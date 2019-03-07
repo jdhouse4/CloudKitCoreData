eaa3474766a9c181e2484af528a9e40a1187b176
Jim Hillhouse <jdhouse4@me.com>
2019-03-06 20:46:30 -0600

    Initial Commit

---
+++ CloudKitCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitCoreData
+//
+//  Created by James Hillhouse IV on 3/6/19.
+//  Copyright © 2019 PortableFrontier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
