eb1495221b3273b8d917d67939c868dcb239e9e1
Jim Hillhouse <jdhouse4@me.com>
2019-03-16 10:28:31 -0500

    Deleted Unneeded Items
    These were for the previous example for KVO.

--- CloudKitCoreData/CountriesViewController.swift
+++ CloudKitCoreData/CountriesViewController.swift
@@ -16,18 +16,10 @@ class CountriesViewController: UIViewController
 {
 
 
-    @IBOutlet weak var stepper: UIStepper!
-    @IBOutlet weak var counter: UILabel!
-    var kvStorage: NSUbiquitousKeyValueStore!
-
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        kvStorage       = NSUbiquitousKeyValueStore()
-        let control     = kvStorage.double(forKey: "control")
-        stepper.value   = control
-        counter.text    = String(control)
 
         let center      = NotificationCenter.default
         center.addObserver(self, selector: #selector(valueReceived(notification:)), name: NSUbiquitousKeyValueStore.didChangeExternallyNotification, object: kvStorage)
@@ -36,20 +28,5 @@ class CountriesViewController: UIViewController
 
     // MARK: Target and Action Functions
 
-    @IBAction func changeValue(_ sender: UIStepper)
-    {
-        let current     = stepper.value
-        counter.text    = String(current)
-        kvStorage.set(current, forKey: "control")
-        kvStorage.synchronize()
-    }
-
-
-    @objc func valueReceived(notification: Notification)
-    {
-        let control     = kvStorage.double(forKey: "control")
-        stepper.value   = control
-        counter.text    = String(control)
-    }
 }
 
