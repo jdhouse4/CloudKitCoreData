326f9a9087617965b49766b5249b2efebd2b35b7
Jim Hillhouse <jdhouse4@me.com>
2019-03-31 23:02:58 -0500

    Hooking Things Up

---
+++ MasterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MasterViewController.swift
+//  CloudKitCoreData
+//
+//  Created by James Hillhouse IV on 3/31/19.
+//  Copyright © 2019 PortableFrontier. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class MasterViewController: UIViewController
+{
+
+    private var countriesTableViewController: CountriesTableViewController
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let countriesController = children.first as? CountriesTableViewController else {
+            fatalError("Check storyboard for missing CountriesTableViewController")
+        }
+
+        countriesTableViewController = countriesController
+
+        //countriesController.delegate = self
+    }
+}
